d2d15447ccd4b08b41e665f0ad76acd3a21aeebd
Rowan Molony <rowanmolony@gmail.com>
2024-07-25 10:43:53 +0100

    schedule battery

--- create.sh
+++ create.sh
@@ -4,10 +4,16 @@ apt-get install jq # for parsing JSON
 flexmeasures db-ops reset
 
 
-flexmeasures add account --name "powerscopeit"
+flexmeasures add account \
+  --name "powerscopeit"
 
 
-flexmeasures add user --username rowan --email rowan@powerscopeit.com --account-id 1 --roles=admin
+flexmeasures add user \
+  --username rowan \
+  --email rowan@powerscopeit.com \
+  --account 1 \
+  --timezone "Europe/Dublin" \
+  --roles=admin
 
 
 flexmeasures add initial-structure
@@ -40,24 +46,7 @@ TOKEN=$(curl \
 
 flexmeasures show asset-types
 
-flexmeasures add asset-type --name "site"
-
-# Asset = Site
-curl \
-  --header "Content-Type: application/json" \
-  --header "Authorization: $TOKEN" \
-  --data '
-  {
-    "name": "Site",
-    "generic_asset_type_id": 8,
-    "account_id": 1,
-    "latitude": 53.3498,
-    "longitude": 6.2603
-  }' \
-  http://localhost:5000/api/v3_0/assets
-
 # Asset = Building
-# Parent = Site
 curl \
   --header "Content-Type: application/json" \
   --header "Authorization: $TOKEN" \
@@ -66,7 +55,6 @@ curl \
     "name": "Building",
     "generic_asset_type_id": 6,
     "account_id": 1,
-    "parent_asset_id": 1,
     "latitude": 53.3498,
     "longitude": 6.2603
   }' \
@@ -82,7 +70,7 @@ curl \
     "name": "Solar",
     "generic_asset_type_id": 1,
     "account_id": 1,
-    "parent_asset_id": 2,
+    "parent_asset_id": 1,
     "latitude": 53.3498,
     "longitude": 6.2603
   }' \
@@ -98,10 +86,9 @@ curl \
     "name": "Battery",
     "generic_asset_type_id": 5,
     "account_id": 1,
-    "parent_asset_id": 2,
+    "parent_asset_id": 1,
     "latitude": 53.3498,
-    "longitude": 6.2603,
-    "attributes": "{\"capacity_in_mw\": 1.5, \"min_soc_in_mwh\": 0.15, \"max_soc_in_mwh\": 1.35}"
+    "longitude": 6.2603
   }' \
   http://localhost:5000/api/v3_0/assets
 
@@ -116,7 +103,7 @@ curl \
 # ************************************************
 
 # Sensor = ElectricityPrices
-# Asset = Site
+# Asset = Building
 curl \
   --header "Content-Type: application/json" \
   --header "Authorization: $TOKEN" \
@@ -139,7 +126,7 @@ curl \
     "name": "power",
     "event_resolution": "PT1H",
     "unit": "MW",
-    "generic_asset_id": 2
+    "generic_asset_id": 1
   }' \
   http://localhost:5000/api/v3_0/sensors
 
@@ -153,7 +140,7 @@ curl \
     "name": "power",
     "event_resolution": "PT1H",
     "unit": "MW",
-    "generic_asset_id": 3
+    "generic_asset_id": 2
   }' \
   http://localhost:5000/api/v3_0/sensors
 
@@ -165,10 +152,11 @@ curl \
   --data '
   {
       "name": "discharging",
-      "event_resolution": "PT1H",
+      "event_resolution": "PT15M",
       "unit": "MW",
-      "generic_asset_id": 1,
-      "event_resolution": "P1D"
+      "generic_asset_id": 3,
+      "event_resolution": "P1D",
+      "attributes": "{\"capacity_in_mw\": 1.5, \"min_soc_in_mwh\": 0.15, \"max_soc_in_mwh\": 1.35}"
   }' \
   http://localhost:5000/api/v3_0/sensors
 
@@ -177,16 +165,54 @@ curl \
 # -----------------
 
 # Sensor = ElectricityPrices
-flexmeasures add beliefs ./fixed_price_feed_in.csv --sensor 1 --source rowan --unit EUR/MWh --timezone Europe/Dublin
+flexmeasures add beliefs ./fixed_price_feed_in.csv \
+  --sensor 1 \
+  --source rowan \
+  --unit EUR/MWh \
+  --timezone Europe/Dublin
 # Plot ->
-flexmeasures show beliefs --sensor 1 --start $START --duration $DURATION
+flexmeasures show beliefs \
+  --sensor 1 \
+  --start $START \
+  --duration $DURATION
 
-# Sensor = BuildingDemand
-flexmeasures add beliefs ./demand.csv --sensor 2 --source rowan --unit MW --timezone Europe/Dublin 
-# Plot ->
-flexmeasures show beliefs --sensor 2 --start $START --duration $DURATION
+# # Sensor = BuildingDemand
+# flexmeasures add beliefs ./demand.csv \
+#   --sensor 2 \
+#   --source rowan \
+#   --unit MW \
+#   --timezone Europe/Dublin 
+# # Plot ->
+# flexmeasures show beliefs \
+#   --sensor 2 \
+#   --start $START \
+#   --duration $DURATION
 
-# Sensor = SolarGeneration
-flexmeasures add beliefs ./mean_solar.csv --sensor 3 --source rowan --unit MW --timezone Europe/Dublin 
+# # Sensor = SolarGeneration
+# flexmeasures add beliefs ./mean_solar.csv \
+#   --sensor 3 \
+#   --source rowan \
+#   --unit MW \
+#   --timezone Europe/Dublin 
+# # Plot ->
+# flexmeasures show beliefs \
+#   --sensor 3 \
+#   --start $START \
+#   --duration $DURATION
+
+
+# Schedule the Battery
+# --------------------
+
+flexmeasures add schedule for-storage \
+    --sensor 4 \
+    --consumption-price-sensor 1 \
+    --start $START \
+    --duration $DURATION \
+    --soc-at-start 50% \
+    --roundtrip-efficiency 90%
 # Plot ->
-flexmeasures show beliefs --sensor 3 --start $START --duration $DURATION
+flexmeasures show beliefs \
+  --sensor 4 \
+  --start $START \
+  --duration $DURATION
