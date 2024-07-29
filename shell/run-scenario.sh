80080e7ae0a12236799fdb441c5fae5d3a787ccf
Rowan Molony <rowanmolony@gmail.com>
2024-07-29 10:57:30 +0100

    wip

--- shell/run-scenario.sh
+++ shell/run-scenario.sh
@@ -8,14 +8,14 @@ BATTERY_ID=3
 FEED_IN_PRICE_ID=1
 FEED_OUT_PRICE_ID=2
 SOLAR_ID=4
-SCHEDULE_ID=5
 
-SITE_POWER_CAPACITY_MW=1.25MW
-SITE_PRODUCTION_CAPACITY_MW=0MW
-SITE_CONSUMPTION_CAPACITY_MW=1.1MW
+SITE_POWER_CAPACITY_MW=1.25
+SITE_PRODUCTION_CAPACITY_MW=0
+SITE_CONSUMPTION_CAPACITY_MW=1.1
 ROUNDTRIP_EFFICIENCY=90%
 INITIAL_SOC=10%
-BATTERY_CAPACITY_MW=0.5MW
+
+BATTERY_CAPACITY_MW=0.5
 BATTERY_MIN_SOC_MWH=1.35 # = 10% * 1.5MWh
 BATTERY_MAX_SOC_MWH=0.15 # = 90% * 1.5MWh
 
@@ -39,25 +39,6 @@ TOKEN=$(curl \
   | sed 's|"||g' \
   )
 
-
-# Update Battery SOC
-# ------------------
-
-CONTENT=$(jq -n \
-  --arg max_soc_in_mwh $BATTERY_MAX_SOC_MWH \
-  --arg min_soc_in_mwh $BATTERY_MIN_SOC_MWH \
-  '$ARGS.named'
-)
-JSON=$(jq -n \
-  --arg attributes "$CONTENT" \
-  '$ARGS.named'
-)
-curl \
-  --request PATCH \
-  --header "Content-Type: application/json" \
-  --header "Authorization: $TOKEN" \
-  --data "$JSON" \
-  "http://localhost:5000/api/v3_0/assets/$BATTERY_ID"
 
 # Create DataSource
 # -----------------
@@ -105,24 +86,40 @@ flexmeasures show beliefs \
   --duration $DURATION
 
 
+# Update Battery SOC
+# ------------------
+
+CONTENT=$(jq \
+  --arg capacity 1.35 \
+  --arg min $BATTERY_MIN_SOC_MWH \
+  --arg max $BATTERY_MAX_SOC_MWH \
+  -n '{capacity_in_mw: $capacity|tonumber, min_soc_in_mwh: $min|tonumber, max_soc_in_mwh: $max|tonumber}'
+)
+JSON=$(jq \
+  --arg attributes "$CONTENT" \
+  -n '{attributes: $attributes}'
+)
+curl \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: $TOKEN" \
+  --data "$JSON" \
+  "http://localhost:5000/api/v3_0/assets/$BATTERY_ID"
+
+
 # Schedule the Battery
 # --------------------
 
 flexmeasures add schedule for-storage \
     --sensor $BATTERY_ID \
     --consumption-price-sensor $FEED_IN_PRICE_ID \
-    --production-price-sensor $FEED_OUT_PRICE_ID \
     --inflexible-device-sensor $SOLAR_ID \
-    --site-power-capacity $SITE_POWER_CAPACITY_MW \
-    --site-production-capacity $SITE_PRODUCTION_CAPACITY_MW \
-    --site-consumption-capacity $SITE_CONSUMPTION_CAPACITY_MW \
-    --site-power-capacity $BATTERY_CAPACITY_MW \
     --start $START \
     --duration $DURATION \
     --soc-at-start $INITIAL_SOC \
     --roundtrip-efficiency $ROUNDTRIP_EFFICIENCY 
 # Plot ->
 flexmeasures show beliefs \
-  --sensor $SCHEDULE_ID \
+  --sensor $BATTERY_ID \
   --start $START \
   --duration $DURATION
